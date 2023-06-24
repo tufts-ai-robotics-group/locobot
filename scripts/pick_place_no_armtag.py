572d5a7204b9a4dbe62f48b0e9cf318d4ea974c1
Yichen Wei <my.waymao+git@gmail.com>
2023-06-24 14:22:58 -0400

    put the object into a bin

--- scripts/pick_place_no_armtag.py
+++ scripts/pick_place_no_armtag.py
@@ -1,6 +1,7 @@
 import math
 import time
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
+from interbotix_perception_modules.apriltag import InterbotixAprilTagInterface
 
 # This script uses the perception pipeline to pick up objects and place them in some virtual basket on the left side of the robot
 # It also uses the AR tag on the arm to get a better idea of where the arm is relative to the camera (though the URDF is pretty accurate already).
@@ -13,29 +14,66 @@ def main():
     bot = InterbotixLocobotXS("locobot_wx200", arm_model="mobile_wx200")
     # move camera such that it's tilting down
     bot.camera.pan_tilt_move(0, 0.75)
-    # get the positions of any clusters present w.r.t. the 'locobot/arm_base_link'
-    # sort the clusters such that they appear from left-to-right w.r.t. the 'locobot/arm_base_link'
-    success, clusters = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
+
+    # move the arm to the sleep pose
+    # bot.arm.set_ee_pose_components(x=0.3, z=0.2) # safe pose
+    # bot.arm.go_to_sleep_pose()
+
+    # Detection of the box with april tags
+    ati = InterbotixAprilTagInterface(
+                    apriltag_ns="locobot" + "/apriltag",
+                    init_node=False,
+                    verbose=True)
+    print("April Tag Interface")
+    ati.valid_tags.append(0)
+    pose = ati.find_pose()
+    print("april tag position:", pose.position)
+    print()
+
+    # x_b, y_b, z_b = pose.position
+    # x_b, y_b, z_b = pose.position.x, pose.position.y, pose.position.z
+    # transform = bot.armtag.find_ref_to_arm_base_transform(position_only=True)
+    # print(transform)
+    # transform = bot.armtag.get_transform(tfBuffer = , target_frame = , source_frame = )
+    # bot.arm.set_ee_pose_components(x=x_b, y=y_b, z=z_b+0.1, pitch=0.5)
+    # time.sleep(10)
+    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
+    # bot.arm.go_to_sleep_pose()
+    # return
+
     # move the robot back so it's centered and open the gripper
     bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
     bot.gripper.open()
 
-    # pick up each object from left-to-right and drop it in a virtual basket on the left side of the robot
-    for cluster in clusters:
+    # get the positions of any clusters present w.r.t. the 'locobot/arm_base_link'
+    # sort the clusters such that they appear from left-to-right w.r.t. the 'locobot/arm_base_link'
+    success, clusters = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="z", reverse=True)
+    print("clusters:", clusters)
+
+    while len(clusters) > 0:
+        cluster = clusters[0]
+        bot.arm.set_ee_pose_components(x=0.3, z=0.2)
         x, y, z = cluster["position"]
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-        time.sleep(0.5)
-        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.5)
-        time.sleep(0.5)
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.8)
+        time.sleep(0.2)
+        bot.arm.set_ee_pose_components(x=x, y=y, z=z, pitch=0.8)
+        time.sleep(0.2)
         bot.gripper.close()
-        time.sleep(0.5)
+        time.sleep(0.2)
 
         bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=0.5)
-        time.sleep(0.5)
-        # bot.arm.set_ee_pose_components(y=0.3, z=0.2)
+        time.sleep(0.2)
+        bot.arm.set_ee_pose_components(x=0.4, y=-0.3, z=0.2, moving_time=1.5)
+        time.sleep(0.2)
         bot.gripper.open()
-        time.sleep(0.5)
+        time.sleep(0.2)
         print("did pickup", cluster)
+
+        bot.arm.set_ee_pose_components(x=0.3, z=0.2, moving_time=1.5)
+        bot.arm.go_to_sleep_pose()
+        time.sleep(0.1)
+        success, clusters = bot.pcl.get_cluster_positions(ref_frame="locobot/arm_base_link", sort_axis="y", reverse=True)
+
     bot.arm.set_ee_pose_components(x=0.3, z=0.2)
     bot.arm.go_to_sleep_pose()
 

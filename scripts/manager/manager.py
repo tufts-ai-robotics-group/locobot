17a2cf349c12132bea75721ebc12bba47d7b96e6
Bharat Kesari <bkesari1998@gmail.com>
2023-06-24 15:58:03 -0400

    Added comments outlining __init__

---
+++ scripts/manager/manager.py
@@ -0,0 +1,41 @@
+import rospy
+from std_msgs.msgs import String
+
+class Manager(object):
+
+    def __init__(self):
+
+        # Have way to start RL without failure for testing
+
+        # Verify that pddl actions have corresponding action servers
+        # Verify that primitve actions have corresponding action servers
+        # Build action clients for both primitive and pddl actions
+        
+        # Load pddl problem and domain file
+        # Create a plan file
+
+        # Loop through actions in plan file
+        # 1. Grab an action from plan file
+        # 2. Parse the action, 1st is the action name, next is each argument
+        # 3. Pass arguments as strings in order to the action
+        # 4. Catch error if there is one
+        # 5. Have ability to query action state by user
+        # 6. Continue untill failure
+
+        # If there is failure, search for action executor in folder
+        # Execute executor if it exists, otherwise instantiate learner
+
+        # Instatiate Learner
+        # 1. Verify that learning agent ouput is same size as RL action space
+        # 2. Verify that learning agent input is same size as observation space
+        # 3. Define RL based on some parameters
+
+        # Loop until RL is complete
+        # 1. Get observation through some API
+        # 2. Send observation to RL
+        # 3. Get action
+        # 4. Execute action
+
+        # Loop to begining
+        pass
+
